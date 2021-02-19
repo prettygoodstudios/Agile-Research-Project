5f3d208887ffd0df0e164be571c19fa0a7cabe0d
Miguel Rust <hava12soft@gmail.com>
2021-02-18 20:28:55 -0700

    Fixed tooltip bug

--- src/components/Conclude.jsx
+++ src/components/Conclude.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Conclude = () => {
     return <div className="section conclude">
         <h1 className="section__header">To conclude</h1>
-        <p>Agile is a management philosophy and methodology that when properly and appropriately implemented can make an organization more nimble and can eliminate wasteful processes. Agile indeed provides legitimate value to the organizations and individuals who implement it. Infact I was able to implement Agile principles and values to develop this website in a time effective manner.</p>
+        <p>Agile is a management philosophy and methodology that when properly and appropriately implemented can make an organization more nimble and can eliminate wasteful processes. Agile indeed provides legitimate value to the organizations and individuals who implement it. Infact I was personally able to implement Agile principles and values to develop this website in a time effective manner.</p>
     </div>
 }
 
